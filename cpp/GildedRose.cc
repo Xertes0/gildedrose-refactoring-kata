309b068a83224c493850b10d7360a7101946c13e
Piotr Stefański <55015543+Xertes0@users.noreply.github.com>
2022-12-10 09:20:27 +0100

    rewrite updateQuality()

--- cpp/GildedRose.cc
+++ cpp/GildedRose.cc
@@ -1,5 +1,6 @@
 #include "GildedRose.h"
 
+#include <functional>
 
 GildedRose::GildedRose(::std::vector<Item> const &items) :
     items(items) {}
@@ -9,53 +10,49 @@ GildedRose::GildedRose(::std::vector<Item> &&items) :
 
 void GildedRose::updateQuality()
 {
-    for(int i = 0; i < items.size(); i++) {
-        if(items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-            if(items[i].quality > 0) {
-                if(items[i].name != "Sulfuras, Hand of Ragnaros") {
-                    items[i].quality = items[i].quality - 1;
-                }
-            }
-        } else {
-            if(items[i].quality < 50) {
-                items[i].quality = items[i].quality + 1;
+    // Special cases
+    static constexpr char const* BACKSTAGE_PASS = "Backstage passes to a TAFKAL80ETC concert";
+    static constexpr char const* BRIE = "Aged Brie";
+    static constexpr char const* SULFURAS = "Sulfuras, Hand of Ragnaros";
 
-                if(items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
-                    if(items[i].sellIn < 11) {
-                        if(items[i].quality < 50) {
-                            items[i].quality = items[i].quality + 1;
-                        }
-                    }
-
-                    if(items[i].sellIn < 6) {
-                        if(items[i].quality < 50) {
-                            items[i].quality = items[i].quality + 1;
-                        }
-                    }
-                }
-            }
-        }
-
-        if(items[i].name != "Sulfuras, Hand of Ragnaros") {
-            items[i].sellIn = items[i].sellIn - 1;
-        }
-
-        if(items[i].sellIn < 0) {
-            if(items[i].name != "Aged Brie") {
-                if(items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                    if(items[i].quality > 0) {
-                        if(items[i].name != "Sulfuras, Hand of Ragnaros") {
-                            items[i].quality = items[i].quality - 1;
-                        }
-                    }
-                } else {
-                    items[i].quality = items[i].quality - items[i].quality;
-                }
+    for(auto& item : items) {
+        // For regular items std::minus
+        // For aged brie std::plus
+        auto regular = [&item](auto sign){
+            if(item.sellIn > 0) {
+                item.quality = sign(item.quality, 1);
             } else {
-                if(items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
-                }
+                item.quality = sign(item.quality, 2);
             }
+        };
+
+        if(item.name == BRIE) {
+            // aged brie has the opposite rules from regular
+            regular(std::plus<>{});
+        } else if(item.name == BACKSTAGE_PASS) {
+            if(item.sellIn > 5) {
+                item.quality += 2;
+            } else if(item.sellIn > 0) {
+                item.quality += 3;
+            } else {
+                item.quality = 0;
+            }
+        } else if(item.name == SULFURAS) {
+            // sellIn is decreased at the end of the function
+            // but has to stay the same for this item
+            item.sellIn += 1;
+        } else {
+            // Generic rules
+            regular(std::minus<>{});
+        }
+
+        // Generic rules
+        item.sellIn -= 1;
+        if(item.quality < 0) {
+            item.quality = 0;
+        }
+        if(item.name != SULFURAS && item.quality > 50) {
+            item.quality = 50;
         }
     }
 }
